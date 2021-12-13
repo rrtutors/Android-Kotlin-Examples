7edac4e94b600621be97a995535dfcc4f010314b
Nisha Jain <nisha.jain@grayopus.com>
2021-12-13 20:00:02 +0530

    CircleImageView and Shadow Effect On Text

---
+++ app/src/main/java/com/nishajain/kotinexamples/ShadowEffectText.kt
@@ -0,0 +1,11 @@
+package com.nishajain.kotinexamples
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ShadowEffectText : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_shadow_effect_text)
+    }
+}
