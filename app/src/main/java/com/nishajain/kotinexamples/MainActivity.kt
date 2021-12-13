0f7391b74edcfdcdd868bd83b4278d9d5acdff7f
Nisha Jain <nisha.jain@grayopus.com>
2021-12-13 10:20:56 +0530

    Initial Commit

---
+++ app/src/main/java/com/nishajain/kotinexamples/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.nishajain.kotinexamples
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import androidx.cardview.widget.CardView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
